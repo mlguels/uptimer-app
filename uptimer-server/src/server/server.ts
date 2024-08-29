d8f28dd4eaacbca6f0a6a8f07ae84315ae24aa75
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-08-28 18:13:06 -0700

    Init apollo server setup

--- uptimer-server/src/server/server.ts
+++ uptimer-server/src/server/server.ts
@@ -1,18 +1,58 @@
 import http from "http";
 
 import { Express, NextFunction, Request, Response } from "express";
-import { PORT } from "./config";
+import { NODE_ENV, PORT } from "./config";
+import { ApolloServer } from "@apollo/server";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { ApolloServerPluginLandingPageDisabled } from "@apollo/server/plugin/disabled";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
+
+const typeDefs = `#graphql
+  type User {
+    username: String
+  }
+
+  type Query {
+    user: User
+  }
+`;
+
+const resolvers = {
+  Query: {
+    user() {
+      return {
+        username: "Danny",
+      };
+    },
+  },
+};
 
 export default class MonitorServer {
   private app: Express;
   private httpServer: http.Server;
+  private server: ApolloServer;
 
   constructor(app: Express) {
     this.app = app;
     this.httpServer = new http.Server(app);
+    const schema = makeExecutableSchema({ typeDefs, resolvers });
+    this.server = new ApolloServer({
+      schema,
+      introspection: NODE_ENV !== "production",
+      plugins: [
+        ApolloServerPluginDrainHttpServer({ httpServer: this.httpServer }),
+        NODE_ENV === "production"
+          ? ApolloServerPluginLandingPageDisabled()
+          : ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+      ],
+    });
   }
 
   async start(): Promise<void> {
+    // You must call the strat() method on the Apollo Server instance before passing the instance to expressMiddleware
+    //
+    await this.server.start();
     this.standardMiddleware(this.app);
     this.startServer();
   }
