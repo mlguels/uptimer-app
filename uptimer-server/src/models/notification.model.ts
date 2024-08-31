e74d684e5a8b017d2e151c55658208518375b1cd
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-08-30 18:05:11 -0700

    Add Notification model

---
+++ uptimer-server/src/models/notification.model.ts
@@ -0,0 +1,46 @@
+import { DataTypes, ModelDefined, Optional } from "sequelize";
+import { sequelize } from "@app/server/database";
+import { INotificationDocument } from "@app/interfaces/notification.interface";
+import { UserModel } from "./user.model";
+
+type NotificationCreationAttributes = Optional<
+  INotificationDocument,
+  "id" | "createdAt"
+>;
+
+const NotificationModel: ModelDefined<
+  INotificationDocument,
+  NotificationCreationAttributes
+> = sequelize.define(
+  "notifications",
+  {
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: UserModel,
+        key: "id",
+      },
+    },
+    groupName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    emails: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: Date.now,
+    },
+  },
+  {
+    indexes: [
+      {
+        fields: ["userId"],
+      },
+    ],
+  }
+) as ModelDefined<INotificationDocument, NotificationCreationAttributes>;
+
+export { NotificationModel };
