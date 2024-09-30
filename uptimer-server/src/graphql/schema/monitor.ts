002235dd825aa15b1da1c755f3551b9d6f433afa
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-09-30 16:03:55 -0700

    Add Monitor grapql Schema

---
+++ uptimer-server/src/graphql/schema/monitor.ts
@@ -0,0 +1,115 @@
+import { buildSchema } from "graphql";
+
+export const monitorSchema = buildSchema(`#graphql
+  input Monitor {
+    id: Int
+    name: String!
+    userId: Int!
+    active: Boolean
+    status: Int!
+    frequency: Int!
+    url: String!
+    method: String
+    type: String!
+    alertThreshold: Int!
+    body: String
+    headers: String
+    httpAuthMethod: String
+    basicAuthUser: String
+    basicAuthPass: String
+    bearerToken: String
+    timeout: Int
+    redirects: Int
+    responseTime: String
+    statusCode: String
+    contentType: String
+    connection: String
+    port: Int
+    notificationId: Int!
+  }
+
+  type HeartBeatResponse {
+    id: ID
+    monitorId: Int
+    status: Int
+    code: Int
+    message: String
+    timestamp: String
+    reqHeaders: String
+    resHeaders: String
+    reqBody: String
+    resBody: String
+    responseTime: Int
+    connection: String
+  }
+
+  input NotificationResult {
+    id: ID!
+    userId: Int!
+    groupName: String!
+    emails: String!
+  }
+
+  type MonitorResult {
+    id: Int
+    name: String!
+    userId: Int!
+    active: Boolean!
+    status: Int!
+    frequency: Int!
+    url: String!
+    method: String
+    type: String!
+    alertThreshold: Int!
+    body: String
+    headers: String
+    httpAuthMethod: String
+    basicAuthUser: String
+    basicAuthPass: String
+    bearerToken: String
+    timeout: Int
+    redirects: Int
+    responseTime: Int
+    statusCode: String
+    contentType: String
+    connection: String
+    lastChanged: String
+    port: Int
+    uptime: Int
+    heartbeats: [HeartBeat!]!
+    notificationId: Int!
+  }
+
+  input ToggleMonitor { 
+    monitorId: Int!
+    userId: Int!
+    name: String!
+    active: Boolean!
+  }
+
+  type MonitorResponse {
+    userId: Int
+    monitors: [MonitorResult!]!
+  }
+
+  type DeleteMonitorResponse {
+    id: ID!
+  }
+
+  type AutoRefresh {
+    refresh: Boolean
+  }
+
+  type Query {
+    getSingleMonitor(monitorId: String!): MonitorResponse
+    getUserMonitors(monitorId: String!): MonitorResponse
+    autoRefresh(userId: String!, refresh: Boolean) AutoRefresh
+  }
+
+  type Mutation {
+    createMonitor(monitor: Monitor!) MonitorResponse
+    toggleMonitor(monitor: ToggleMonitor!) MonitorResponse
+    updateMonitor(monitorId: ID!, userId: ID!, monitor: Monitor!) MonitorResponse
+    deleteMonitor(monitorId: ID!, userId: ID!, type: String!) DeleteMonitorResponse
+  }
+`);
