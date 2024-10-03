95aa0af7d4a224a5596582f0458241aec593e1d9
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-10-03 16:47:49 -0700

    Add createMonitor mutation resolver

--- uptimer-server/src/graphql/resolvers/monitor.ts
+++ uptimer-server/src/graphql/resolvers/monitor.ts
@@ -1,4 +1,5 @@
 import { AppContext, IMonitorArgs, IMonitorDocument } from "@app/interfaces/monitor.interface";
+import logger from "@app/server/logger";
 import { createMonitor } from "@app/services/monitor.service";
 import { authenticateGraphQLRoute } from "@app/utils/utils";
 
@@ -11,8 +12,9 @@ export const MonitorResolver = {
       const body: IMonitorDocument = args.monitor!;
       const monitor: IMonitorDocument = await createMonitor(body);
 
-      if (body.active && monitor.active) {
+      if (body.active && monitor?.active) {
         // TODO: start created monitor
+        logger.info("Start new monitor");
       }
 
       return {
--- uptimer-server/src/graphql/schema/index.ts
+++ uptimer-server/src/graphql/schema/index.ts
@@ -1,5 +1,6 @@
 import { mergeTypeDefs } from "@graphql-tools/merge";
 import { userSchema } from "./user";
 import { notificationSchema } from "./notification";
+import { monitorSchema } from "./monitor";
 
-export const mergedGQLSchema = mergeTypeDefs([userSchema, notificationSchema]);
+export const mergedGQLSchema = mergeTypeDefs([userSchema, notificationSchema, monitorSchema]);
