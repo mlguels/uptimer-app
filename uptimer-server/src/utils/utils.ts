151c244dafd399840de54fa181c0a75e68d3c3b6
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-10-15 21:49:01 -0700

    Add sleep, randomInt, startMonitors and resume monitors util functions

--- uptimer-server/src/utils/utils.ts
+++ uptimer-server/src/utils/utils.ts
@@ -1,8 +1,12 @@
-import { IAuthPayload } from "@app/interfaces/user.interface";
-import { JWT_TOKEN } from "@app/server/config";
 import { Request } from "express";
 import { GraphQLError } from "graphql";
+import { toLower } from "lodash";
 import { verify } from "jsonwebtoken";
+
+import { IMonitorDocument } from "@app/interfaces/monitor.interface";
+import { IAuthPayload } from "@app/interfaces/user.interface";
+import { JWT_TOKEN } from "@app/server/config";
+import { getAllUsersActiveMonitors, getMonitorById, startCreatedMonitors } from "@app/services/monitor.service";
 
 export const appTimeZone: string = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
@@ -34,4 +38,40 @@ export const authenticateGraphQLRoute = (req: Request): void => {
   } catch (error) {
     throw new GraphQLError("Please login again");
   }
+};
+
+/**
+ * Delay for specified number of seconds
+ * @param ms Number of milliseconds to sleep for
+ * @returns {Promise<void>}
+ */
+export const sleep = (ms: number): Promise<void> => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+/**
+ * Returns random integer between min and max (both inclusive)
+ * @param min
+ * @param max
+ * @returns {number}
+ */
+
+export const getRandomInt = (min: number, max: number): number => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export const startMonitors = async (): Promise<void> => {
+  const list: IMonitorDocument[] = await getAllUsersActiveMonitors();
+
+  for (const monitor of list) {
+    startCreatedMonitors(monitor, toLower(monitor.name), monitor.type);
+    await sleep(getRandomInt(300, 1000));
+  }
+};
+export const resumeMonitors = async (monitorId: number): Promise<void> => {
+  const monitor: IMonitorDocument = await getMonitorById(monitorId);
+  startCreatedMonitors(monitor, toLower(monitor.name), monitor.type);
+  await sleep(getRandomInt(300, 1000));
 };
