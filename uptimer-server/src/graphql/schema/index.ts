8129bf2c3fb1b15dbf34cb22931295e0256b44b2
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-10-30 18:27:07 -0700

    Add heratbeat tp typedefs

--- uptimer-server/src/graphql/schema/index.ts
+++ uptimer-server/src/graphql/schema/index.ts
@@ -2,5 +2,6 @@ import { mergeTypeDefs } from "@graphql-tools/merge";
 import { userSchema } from "./user";
 import { notificationSchema } from "./notification";
 import { monitorSchema } from "./monitor";
+import { heartbeatSchema } from "./heartbeats";
 
-export const mergedGQLSchema = mergeTypeDefs([userSchema, notificationSchema, monitorSchema]);
+export const mergedGQLSchema = mergeTypeDefs([userSchema, notificationSchema, monitorSchema, heartbeatSchema]);
