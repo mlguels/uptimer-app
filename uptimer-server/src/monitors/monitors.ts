1c4f8c350c003ede3d75c8055fc1b41dbc8b68fd
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-11-26 16:19:02 -0800

    Add redis ping method

--- uptimer-server/src/monitors/monitors.ts
+++ uptimer-server/src/monitors/monitors.ts
@@ -1,5 +1,6 @@
-import { IMonitorResponse } from "@app/interfaces/monitor.interface";
 import { MongoClient } from "mongodb";
+import { createClient } from "redis";
+import { IMonitorResponse } from "@app/interfaces/monitor.interface";
 
 export const mongodbPing = async (connectionString: string): Promise<IMonitorResponse> => {
   const startTime: number = Date.now();
@@ -33,5 +34,56 @@ export const mongodbPing = async (connectionString: string): Promise<IMonitorRes
           });
         }
       });
+  });
+};
+
+export const redisPing = async (connectionString: string): Promise<IMonitorResponse> => {
+  const startTime: number = Date.now();
+  return new Promise((resolve, reject) => {
+    const client = createClient({
+      url: connectionString,
+    });
+    client.on("error", (error) => {
+      if (client.isOpen) {
+        client.disconnect();
+      }
+      reject({
+        status: "refused",
+        responseTime: Date.now() - startTime,
+        message: error.message ?? "Redis connection refused",
+        code: 500,
+      });
+    });
+    client.connect().then(() => {
+      if (!client.isOpen) {
+        reject({
+          status: "refused",
+          responseTime: Date.now() - startTime,
+          message: "Connection isn't open",
+          code: 500,
+        });
+        client
+          .ping()
+          .then(() => {
+            if (client.isOpen) {
+              client.disconnect();
+            }
+            resolve({
+              status: "established",
+              responseTime: Date.now() - startTime,
+              message: "Redis server running",
+              code: 200,
+            });
+          })
+          .catch((err) => {
+            reject({
+              status: "refused",
+              responseTime: Date.now() - startTime,
+              message: err.message ?? "Redis server down",
+              code: 500,
+            });
+          });
+      }
+    });
   });
 };
